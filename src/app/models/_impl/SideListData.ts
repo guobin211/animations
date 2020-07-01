a359689bb55fd6a9f71fab0ab34eac5c05a187b1
GuoBin <guobin201314@gmail.com>
2020-07-01 16:37:09 +0800

    feature(models): change SideNavData

--- src/app/models/_impl/SideListData.ts
+++ src/app/models/_impl/SideListData.ts
@@ -5,7 +5,7 @@
 import { SideItem } from "../_base";
 
 export const SideListData: SideItem[] = [
-  {name: "Canvas", path: "/canvas"},
+  {name: "Canvas", path: "/canvas/canvas"},
   {name: "CanvasContext", path: "/canvas/context"},
   {name: "CanvasRenderer", path: "/canvas/renderer"},
   {name: "CanvasImage", path: "/canvas/image"},
