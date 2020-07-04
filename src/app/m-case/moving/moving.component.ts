615d6625d856649e065424cfe8c2051401530377
GuoBin <guobin201314@gmail.com>
2020-07-04 16:29:48 +0800

    feature(case): 拖动

---
+++ src/app/m-case/moving/moving.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-moving",
+  templateUrl: "./moving.component.html",
+  styles: [],
+})
+export class MovingComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
