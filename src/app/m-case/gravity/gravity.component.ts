a86555d983da639957d65aabc20ad2a143380069
GuoBin <guobin201314@gmail.com>
2020-07-04 16:29:34 +0800

    feature(case): 重力

---
+++ src/app/m-case/gravity/gravity.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-gravity",
+  templateUrl: "./gravity.component.html",
+  styles: [],
+})
+export class GravityComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
