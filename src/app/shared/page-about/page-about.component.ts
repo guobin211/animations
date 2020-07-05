549b50fd5406c0b486896615c1699796ef3c542d
GuoBin <guobin201314@gmail.com>
2020-07-05 19:57:33 +0800

    add json with animate

--- src/app/shared/page-about/page-about.component.ts
+++ src/app/shared/page-about/page-about.component.ts
@@ -1,12 +1,45 @@
-import { Component, OnInit } from "@angular/core";
+import { AfterViewInit, Component } from "@angular/core";
 
 @Component({
   selector: "app-page-about",
   templateUrl: "./page-about.component.html",
   styleUrls: ["./page-about.component.scss"],
 })
-export class PageAboutComponent implements OnInit {
+export class PageAboutComponent implements AfterViewInit {
   constructor() {}
 
-  ngOnInit(): void {}
+  ngAfterViewInit() {
+    const animation1 = (window as any).lottie.loadAnimation({
+      container: document.getElementById('lottie1'),
+      path: 'assets/data/26219-new-idea.json',
+      renderer: 'svg/canvas/html',
+      loop: true,
+      autoplay: true,
+      name: "Lottie Animate",
+    });
+    const animation2 = (window as any).lottie.loadAnimation({
+      container: document.getElementById('lottie2'),
+      path: 'assets/data/25546-jumping-feelings.json',
+      renderer: 'svg/canvas/html',
+      loop: true,
+      autoplay: true,
+      name: "Lottie Animate",
+    });
+    const animation3 = (window as any).lottie.loadAnimation({
+      container: document.getElementById('lottie3'),
+      path: 'assets/data/26131-defend-yourself-from-covid-19.json',
+      renderer: 'svg/canvas/html',
+      loop: true,
+      autoplay: true,
+      name: "Lottie Animate",
+    });
+    const animation4 = (window as any).lottie.loadAnimation({
+      container: document.getElementById('lottie4'),
+      path: 'assets/data/26238-delivery-man.json',
+      renderer: 'svg/canvas/html',
+      loop: true,
+      autoplay: true,
+      name: "Lottie Animate",
+    })
+  }
 }
